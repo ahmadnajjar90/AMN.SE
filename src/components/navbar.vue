38f1023da54222a8eaec852c43f84093165b3db6
ahmadnajjar90 <a7mad.najjar@hotmail.com>
2020-12-06 01:02:16 +0100

    first commit

---
+++ src/components/navbar.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <navbar1/>
+    </div>
+</template>
+
+<script>
+import navbar1 from "@/components/navbar/navbar1.vue";
+
+export default {
+    components: {
+        navbar1,
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/navbar/navbar1.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <a href="#"><h1>AMN</h1></a>
+        <form>
+            <input type="text" name="" id="">
+            <input type="button" value="Search">
+        </form>
+        <input type="button" value="basket">
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>

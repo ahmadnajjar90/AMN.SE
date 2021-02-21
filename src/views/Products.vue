e23ae6a3443b2038ac2655deb7e21b0a61303bfd
ahmadnajjar90 <a7mad.najjar@hotmail.com>
2021-02-21 23:30:24 +0100

    21

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,15 +1,18 @@
 <template>
     <div>
+        <shopProduct/>
         <subscribe/>
     </div>
 </template>
 
 <script>
+import shopProduct from '@/components/shopProduct'
 import subscribe from '@/components/subscribe'
 
 export default {
     name: 'Products',
     components: {
+        shopProduct,
         subscribe,
     }
 }
